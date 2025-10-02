bbe08b27554ca08e6ebc31705186a770ceb86d2e
Muhamad Shidiq Maulana <shidiqmuh0@gmail.com>
2025-10-03 00:22:47 +0700

    adding sayuri yellow

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,26 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
+import '../styles/global.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag('config', 'G-RQLP7E1FTL', {
-        page_path: url,
-      });
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
+      if (typeof window.gtag !== 'undefined') {
+        window.gtag('config', 'G-RQLP7E1FTL', { page_path: url })
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
-      {/* Google Analytics Script */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-RQLP7E1FTL"
-      />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RQLP7E1FTL" />
       <Script
         id="google-analytics"
         strategy="afterInteractive"
@@ -39,5 +35,5 @@ export default function App({ Component, pageProps }) {
       />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
